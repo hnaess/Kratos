458325f52297b66813c1beb7ce8f015e1352e7ad
Henrik <hnaess@gmail.com>
2015-05-27 10:09:36 +0200

    Initital

---
+++ KratosApp/Controller/KratosController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using KratosApp.Models;
+using System.Collections.Specialized;
+using System.Web;
+
+namespace KratosApp.Controller
+{
+    public class KratosController : ApiController
+    {
+        Kratos[] kratoses = new Kratos[] 
+        { 
+            new Kratos { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 }, 
+            new Kratos { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M }, 
+            new Kratos { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M } 
+        };
+
+        public IEnumerable<Kratos> Get()
+        {
+            NameValueCollection nvc = HttpUtility.ParseQueryString(Request.RequestUri.Query);
+            var system = nvc["System"];
+            // BL comes here
+            return kratoses;
+        }
+
+        //public IEnumerable<Kratos> GetAllProducts()
+        //{
+        //    return kratoses;
+        //}
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var Kratos = kratoses.FirstOrDefault((p) => p.Id == id);
+            if (Kratos == null)
+            {
+                return NotFound();
+            }
+            return Ok(Kratos);
+        }
+    }
+}
---
+++ KratosApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("KratosApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KratosApp")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ccef28ee-2a04-41b4-84e6-ce099ca84241")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
