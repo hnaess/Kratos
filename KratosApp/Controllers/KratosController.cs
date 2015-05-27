46490b62fdc2d5ed6874df1c6813258f9af6558f
Henrik <hnaess@gmail.com>
2015-05-27 12:09:25 +0200

    Improved

--- KratosApp/Controllers/KratosController.cs
+++ KratosApp/Controllers/KratosController.cs
@@ -21,10 +21,25 @@ namespace KratosApp.Controller
 
         public IEnumerable<Kratos> Get()
         {
-            NameValueCollection nvc = HttpUtility.ParseQueryString(Request.RequestUri.Query);
-            var system = nvc["System"];
-            // BL comes here
-            return kratoses;
+            var kratos = new Kratos()
+            {
+                cad = 90,
+                bike = "racetops",
+                fstg = 7,
+                fCadc = 90,
+                fP = 160,
+                fV = 20, //9.3,
+                fW = 10,
+
+                fh = 172,
+                fM = 71.3,
+                fT = 20,
+                fHn = 350,
+                
+            };
+            kratos.Calc(Kratos.CalcMethod.Power);
+            kratos.Calc(Kratos.CalcMethod.Speed);
+            return new List<Kratos>() { kratos };
         }
 
         //public IEnumerable<Kratos> GetAllProducts()
@@ -32,19 +47,22 @@ namespace KratosApp.Controller
         //    return kratoses;
         //}
 
-        public IHttpActionResult GetProduct(int id)
-        {
-            var nvc = HttpUtility.ParseQueryString(Request.RequestUri.Query);
+        //public IHttpActionResult GetProduct(int id)
+        //{
+        //    var nvc = HttpUtility.ParseQueryString(Request.RequestUri.Query);
 
-            //var Kratos = kratoses.FirstOrDefault((p) => p.Id == id);
-            //if (Kratos == null)
-            //{
-            //    return NotFound();
-            //}
-            //return Ok(Kratos);
+        //    }
+            
 
-            var Kratos = kratoses.First();
-            return Ok(Kratos);
-        }
+        //    //var Kratos = kratoses.FirstOrDefault((p) => p.Id == id);
+        //    //if (Kratos == null)
+        //    //{
+        //    //    return NotFound();
+        //    //}
+        //    //return Ok(Kratos);
+
+        //    var Kratos = kratoses.First();
+        //    return Ok(Kratos);
+        //}
     }
 }
--- KratosApp/Models/Kratos.cs
+++ KratosApp/Models/Kratos.cs
@@ -7,122 +7,99 @@ namespace KratosApp.Models
 {
     public class Kratos
     {
-        public int Power { get; set; }
-        public double Speed { get; set; }
+        public double PowerOutput { get; set; }
+        public double SpeedOutput { get; set; }
+
+        public double cad { get; set; }
+
+        public string bike { get; set; }
+        public double fstg { get; set; }
+        public double fCadc { get; set; }
+        public double fP { get; set; }
+        public double fV { get; set; }
+        public double fW { get; set; }
+
+        public double fh { get; set; }
+        public double fM { get; set; }
+        public double fT { get; set; }
+        public double fHn { get; set; }
+
+        public double fCwA { get; private set; }
+        public double CrEff { get; private set; }
 
 
-    }
 
-    public static class Speed
-    {
-        static int iLang = 0;
-        static bool engl = true;
+        bool isTandem { get { return bike == "tandem"; } }
 
-        public static void Start()
+        static bool imperial = false;
+
+        private int bikeI { get { return asBike.IndexOf(bike); } }
+
+
+        static List<String> asBike = new List<String>() { "roadster", "mtb", "tandem", "racetops", "racedrops", "tria", "superman", "lwbuss", "swbuss", "swbass", "ko4", "ko4tailbox", "whitehawk", "questclosed", "handtrike" };
+        static List<double> afCd = new List<double>() { .95, .79, .35, .82, .60, .53, .47, .85, .67, .60, .50, .41, 0, 0, .62 };
+        static List<double> afSin = new List<double>() { .95, .85, .7, .89, .67, .64, .55, .64, .51, .44, .37, .37, 0, 0, .55 };
+        static List<double> afCdBike = new List<double>() { 2.0, 1.5, 1.7, 1.5, 1.5, 1.25, .90, 1.7, 1.6, 1.25, 1.2, 1.15, .036, .090, 1.5 };
+        static List<double> afAFrame = new List<double>() { .06, .052, .06, .048, .048, .048, .044, .039, .036, .031, .023, .026, 1, 1, .046 };
+        static List<double> afCATireV = new List<double>() { 1.1, 1.1, 1.1, 1.1, 1.1, 1.1, .9, .66, .8, .85, .77, .77, .1, .26, .9 };
+        static List<double> afCATireH = new List<double>() { .9, .9, .9, .9, .9, .7, .7, .9, .80, .84, .49, .3, .13, .16, 2 };
+        static List<double> afLoadV = new List<double>() { .33, .45, .5, .40, .45, .47, .48, .32, .55, .55, .63, .63, .55, .72, .5 };
+        static List<double> afCCrV = new List<double>() { 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.25, 1.25, 1.25, 1.25, 1.25, 1.25, 1.5, 1.5 };
+        static List<double> afCm = new List<double>() { 1.03, 1.025, 1.05, 1.025, 1.025, 1.025, 1.025, 1.04, 1.04, 1.04, 1.05, 1.05, 1.06, 1.09, 1.03 };
+        static List<double> afMBikeDef = new List<double>() { 18, 12, 17.8, 9.5, 9.5, 9.5, 8, 18, 15.5, 11.5, 11.8, 13.5, 18, 32, 18 };
+        static List<int> aiTireFDef = new List<int>() { 3, 5, 1, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0 };
+        static List<int> aiTireRDef = new List<int>() { 3, 5, 1, 0, 0, 0, 0, 3, 3, 0, 0, 0, 0, 0, 0 };
+
+        static List<double> Cr = new List<double>() { .0033, .0031, .0029, .0050, .0016, .0046 };
+        static List<double> ATire = new List<double>() { .021, .031, .042, .048, .042, .055 };
+
+        public enum CalcMethod { Power, Speed };
+
+
+        public void Calc(CalcMethod calcMethod)
         {
-            double prefCad = 90, cCad = .002;
+            const double cCad = .002;
+            
+            int fVRSelectedIndex = 0; // 0 = narrow racing tire (high pressurE)
+            int fHRSelectedIndex = 0;
 
-            string bike = "racetops";
-            double fstg = 7;               // grade;
-            double fCadc = 90;              // cadence
-            double fP = 500;                // power
-            double fV = 9.3;                // speed
-            double fW = 1;                  // wind
+            var fmr = afMBikeDef[bikeI] * (imperial? 2.2 : 1);
 
-            //vr = Front Wheel
-            //hr = Rear Wheel
-
-            //var asBike = new List<String>() { "roadster", "mtb", "tandem", "racetops", "racedrops", "tria", "superman", "lwbuss", "swbuss", "swbass", "ko4", "ko4tailbox", "whitehawk", "questclosed", "handtrike" };
-            var afCd = new List<double>() { .95, .79, .35, .82, .60, .53, .47, .85, .67, .60, .50, .41, 0, 0, .62 };
-            var afSin = new List<double>() { .95, .85, .7, .89, .67, .64, .55, .64, .51, .44, .37, .37, 0, 0, .55 };
-            var afCdBike = new List<double>() { 2.0, 1.5, 1.7, 1.5, 1.5, 1.25, .90, 1.7, 1.6, 1.25, 1.2, 1.15, .036, .090, 1.5 };
-            var afAFrame = new List<double>() { .06, .052, .06, .048, .048, .048, .044, .039, .036, .031, .023, .026, 1, 1, .046 };
-            var afCATireV = new List<double>() { 1.1, 1.1, 1.1, 1.1, 1.1, 1.1, .9, .66, .8, .85, .77, .77, .1, .26, .9 };
-            var afCATireH = new List<double>() { .9, .9, .9, .9, .9, .7, .7, .9, .80, .84, .49, .3, .13, .16, 2 };
-            var afLoadV = new List<double>() { .33, .45, .5, .40, .45, .47, .48, .32, .55, .55, .63, .63, .55, .72, .5 };
-            var afCCrV = new List<double>() { 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.25, 1.25, 1.25, 1.25, 1.25, 1.25, 1.5, 1.5 };
-            var afCm = new List<double>() { 1.03, 1.025, 1.05, 1.025, 1.025, 1.025, 1.025, 1.04, 1.04, 1.04, 1.05, 1.05, 1.06, 1.09, 1.03 };
-            var afMBikeDef = new List<double>() { 18, 12, 17.8, 9.5, 9.5, 9.5, 8, 18, 15.5, 11.5, 11.8, 13.5, 18, 32, 18 };
-            var aiTireFDef = new List<int>() { 3, 5, 1, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0 };
-            var aiTireRDef = new List<int>() { 3, 5, 1, 0, 0, 0, 0, 3, 3, 0, 0, 0, 0, 0, 0 };
-
-
-            double fBh, fh;
-            double fBm, fM;
-            double fT;
-            double fHn;
-
-            fh = fBh = (iLang == 1) ? 67.7 : 172;
-            fM = fBm = (iLang == 1) ? 157.2 : 71.3;
-            fT = (iLang == 1) ? 68 : 20;                    // Air Temperature;
-            fHn = (iLang == 1) ? 1150 : 350;                // Height above SeaLevel;
-
-
-            // change bike
-            int i = 3; // racetops
-            var fmr = afMBikeDef[i] * ((iLang == 1) ? 2.2 : 1);
-            int fVRSelectedIndex = aiTireFDef[i];
-            int fHRSelectedIndex = aiTireRDef[i];
-
-            // rest
-
-            var bikeI = 3;                                  // hands on top. TODO, change to bike type
-            //var bikeR = 0;
-
-            var hRider = fh * (engl ? ((iLang == 1) ? .0254 : .01) : 1) * ((bike == "tandem") ? 2 : 1);
-            var M = fM * ((iLang == 1) ? .453592 : 1) * ((bike == "tandem") ? 2 : 1);
-            var MBik = fmr * ((iLang == 1) ? .453592 : 1);
-            var T = (iLang == 1) ? 5 * (fT - 32) / 9 : fT;
-            var Hn = fHn * ((iLang == 1) ? .3048 : 1);
+            var hRider = fh * (imperial ? .0254 : .01) * (isTandem ? 2 : 1);
+            var M = fM * (imperial ? .453592 : 1) * (isTandem ? 2 : 1);
+            var MBik = fmr * (imperial? .453592 : 1);
+            var T = imperial ? 5 * (fT - 32) / 9 : fT;
+            var Hn = fHn * (imperial ? .3048 : 1);
             var Slope = Math.Atan(fstg * .01);
-            var W = fW * ((iLang == 1) ? .44704 : .27778);
-            var P = fP * ((bike == "tandem") ? 2 : 1);
-            var V = fV * ((iLang == 1) ? .44704 : .27778);
-            var cad = fCadc;
-            double diagrCad;
+            var W = fW * (imperial ? .44704 : .27778);
+            var P = fP * (isTandem ? 2 : 1);
+            var V = fV * (imperial ? .44704 : .27778);
 
-            if (!engl) hRider /= ((hRider < 10) ? 1 : ((10 <= hRider && hRider < 400) ? 100 : 1000));
-            fCadc = diagrCad = cad = Math.Abs(cad);
-            if (cad > 10) prefCad = cad;
-
-            var Cr = new List<double>() { .0033, .0031, .0029, .0050, .0016, .0046 };
-            var ATire = new List<double>() { .021, .031, .042, .048, .042, .055 };
             var CrDyn = 0.1 * Math.Cos(Slope);
             var j = fVRSelectedIndex;
             double CrV = Cr[j], ATireV = ATire[j];
             j = fHRSelectedIndex;
             double CrH = Cr[j], ATireH = ATire[j];
-            var CrEff = afLoadV[bikeI] * afCCrV[bikeI] * CrV + (1.0 - afLoadV[bikeI]) * CrH;
+            CrEff = afLoadV[bikeI] * afCCrV[bikeI] * CrV + (1.0 - afLoadV[bikeI]) * CrH;
 
             var adipos = (bike == "whitehawk" || bike == "questclosed") ? 0 : Math.Sqrt(M / (hRider * 750));
             var CwaBike = afCdBike[bikeI] * (afCATireV[bikeI] * ATireV + afCATireH[bikeI] * ATireH + afAFrame[bikeI]);
-            double CwaRider, Ka, cardB;
+            double CwaRider = 0;
             var Frg = 9.81 * (MBik + M) * (CrEff * Math.Cos(Slope) + Math.Sin(Slope));
 
+            CwaRider = (1 + cad * cCad) * afCd[bikeI] * adipos * (((hRider - adipos) * afSin[bikeI]) + adipos);
+            double Ka = 176.5 * Math.Exp(-Hn * .0001253) * (CwaRider + CwaBike) / (273 + T);
+
             // Calculate based on Power
-            if (fP > 0)
+            //double outputSpeed = 0;
+            if (calcMethod == CalcMethod.Power)
             {
-                if (P > 0 && cad == 0)
-                {
-                    cad = prefCad;
-                    //WRITE_OUTPUT(fCadc, 0, cad);
-                    //alert(P1txt + Ptxt + cad + ".");
-                }
-                if (P <= 0 && cad > 0)
-                {
-                    cad = 0;
-                    //WRITE_OUTPUT(fCadc, 0, 0);
-                    //alert(P0txt + Ptxt + "0.");
-                }
-                CwaRider = (1 + cad * cCad) * afCd[bikeI] * adipos * (((hRider - adipos) * afSin[bikeI]) + adipos);
-                Ka = 176.5 * Math.Exp(-Hn * .0001253) * (CwaRider + CwaBike) / (273 + T);
-                cardB = (3 * Frg - 4 * W * CrDyn) / (9 * Ka) - Math.Pow(CrDyn, 2) / (9 * Math.Pow(Ka, 2)) - (W * W) / 9;
+                double cardB = (3 * Frg - 4 * W * CrDyn) / (9 * Ka) - Math.Pow(CrDyn, 2) / (9 * Math.Pow(Ka, 2)) - (W * W) / 9;
                 double cardA = -((Math.Pow(CrDyn, 3) - Math.Pow(W, 3)) / 27
                             + W * (5 * W * CrDyn + 4 * Math.Pow(CrDyn, 2) / Ka - 6 * Frg) / (18 * Ka)
                             - P / (2 * Ka * afCm[bikeI])
                             - CrDyn * Frg / (6 * Math.Pow(Ka, 2))),
                 sqrt = Math.Pow(cardA, 2) + Math.Pow(cardB, 3),
-                    //ire = cardA - Math.Sqrt(sqrt), Vms;
                 ire = cardA - Math.Sqrt(sqrt); //, Vms;
                 double Vms;
                 if (sqrt >= 0)
@@ -131,7 +108,7 @@ namespace KratosApp.Models
                     Vms = 2 * Math.Sqrt(-cardB) * Math.Cos(Math.Acos(cardA / Math.Sqrt(Math.Pow(-cardB, 3))) / (double)3);
                 Vms -= 2 * W / (double)3 + CrDyn / (double)(3 * Ka);
 
-                double fVoutput = ((iLang == 1) ? 2.2369 : 3.6) * Vms;
+                SpeedOutput = (imperial ? 2.2369 : 3.6) * Vms;
 
                 if (Vms < -W)
                 {
@@ -142,34 +119,17 @@ namespace KratosApp.Models
 
 
             // Caclculate based on Speed
-            if (fV > 0)
+            if (calcMethod == CalcMethod.Speed)
             {
-                // TODO: Remove loop?
-                double y = 7979;
-                while (y == 7979 || Math.Round((double)y) <= 0 && cad > 0 || Math.Round((double)y) > 0 && cad == 0)
+                var vw = V + W;
+                PowerOutput = afCm[bikeI] * V * (Ka * (vw * ((vw < 0) ? -vw : vw)) + Frg + V * CrDyn);
+                if (isTandem)
                 {
-                    if (y != 7979 && Math.Round((double)y) <= 0 && cad > 0)
-                    {
-                        cad = 0;
-                        //WRITE_OUTPUT(fCadc, 0, cad);
-                        //alert(P0txt + Ptxt + "0.");
-                    }
-                    if (y != 7979 && Math.Round((double)y) > 0 && cad == 0)
-                    {
-                        cad = prefCad;
-                        //WRITE_OUTPUT(fCadc, 0, cad);
-                        //alert(P1txt + Ptxt + cad + ".");
-                    }
-                    var vw = V + W;
-                    CwaRider = (1 + cad * cCad) * afCd[bikeI] * adipos * (((hRider - adipos) * afSin[bikeI]) + adipos);
-                    Ka = 176.5 * Math.Exp(-Hn * .0001253) * (CwaRider + CwaBike) / (273 + T);
-                    y = afCm[bikeI] * V * (Ka * (vw * ((vw < 0) ? -vw : vw)) + Frg + V * CrDyn);
-                    if (bike == "tandem")
-                        y *= 0.5;
-
-                    var fPoutput = y;
+                    PowerOutput *= 0.5;
                 }
             }
+
+            fCwA = (CwaBike + CwaRider) * (imperial ? 10.764 : 1);
         }
     }
 }
